5dd507301bf8923a586764fa106b9dd676de9591
jdieramon <jodiera@upv.es>
2016-09-04 16:49:07 -0400

    Create w1-clustering.R

---
+++ Statistics/w1-clustering.R
@@ -0,0 +1,100 @@
+## ----global_palette, results = 'asis'------------------------------------
+rm(list=ls())
+tropical=  c('darkorange', 'dodgerblue', 'hotpink', 'limegreen', 'yellow')
+palette(tropical)
+
+## ----global_options,warning=FALSE,message=FALSE--------------------------
+## see ch. 10 Hooks of Xie's knitr book
+library(knitr)
+knit_hooks$set(setPch = function(before, options, envir) {
+    if(before) par(pch = 19)
+})
+opts_chunk$set(setPch = TRUE)
+
+## ----global_plot,warning=FALSE, message=FALSE----------------------------
+knitr::opts_chunk$set(fig.width=5, fig.height=5, size="footnotesize",
+                      warning=FALSE, message=FALSE)
+knitr::knit_hooks$set(small.mar = function(before, options, envir) {
+    if (before) graphics::par(mar = c(5,5,1.5,1))
+})
+
+## ----load_hidden, echo=FALSE, results="hide", warning=FALSE--------------
+suppressPackageStartupMessages({
+    library(devtools)
+    library(Biobase)
+    library(dendextend)
+})
+
+## ----load----------------------------------------------------------------
+library(devtools)
+library(Biobase)
+library(dendextend)
+
+## ----install_packages, eval=FALSE----------------------------------------
+#  install.packages(c("devtools","dendextend"))
+#  source("http://www.bioconductor.org/biocLite.R")
+#  biocLite(c("Biobase"))
+
+## ------------------------------------------------------------------------
+con =url("http://bowtie-bio.sourceforge.net/recount/ExpressionSets/bodymap_eset.RData")
+load(file=con)
+close(con)
+bm = bodymap.eset
+pdata=pData(bm)
+edata=as.data.frame(exprs(bm))
+fdata = fData(bm)
+ls()
+
+## ------------------------------------------------------------------------
+edata = edata[rowMeans(edata) > 5000,] #require the mean > some value
+dim(edata)
+edata = log2(edata + 1) #transform
+
+# By default calculates the distance between rows
+dist1 = dist(t(edata)) #I want d between samples: transpos
+
+## Look at distance matrix
+colramp = colorRampPalette(c(3,"white",2))(9)
+heatmap(as.matrix(dist1),col=colramp,Colv=NA,Rowv=NA)
+
+## ------------------------------------------------------------------------
+hclust1 = hclust(dist1)
+plot(hclust1)
+
+## ------------------------------------------------------------------------
+plot(hclust1,hang=-1)
+
+## ------------------------------------------------------------------------
+dend = as.dendrogram(hclust1)
+# '''Define clusters: color_labels()
+dend = color_labels(hclust1,4,col=1:4) # define 4 clusters
+plot(dend)
+dend = color_labels(hclust1,3,col=1:3) # define 3 clusters
+plot(dend)
+
+## ------------------------------------------------------------------------
+# '''Define clusters: labels_colors()
+labels_colors(dend) = c(rep(1,10),rep(2,9)) #define labels 10 times label1 + 9 times label2
+plot(dend)
+
+## ------------------------------------------------------------------------
+kmeans1 = kmeans(edata,centers=3)
+names(kmeans1)
+
+## ------------------------------------------------------------------------
+matplot(t(kmeans1$centers),col=1:3,type="l",lwd=3)
+
+## ------------------------------------------------------------------------
+# '''How many genes belong to each cluster?
+kmeans1$cluster[1:10]
+table(kmeans1$cluster)
+
+## ------------------------------------------------------------------------
+heatmap(as.matrix(edata)[order(kmeans1$cluster),],col=colramp,Colv=NA,Rowv=NA)
+
+## ------------------------------------------------------------------------
+kmeans2 = kmeans(edata,centers=3)
+table(kmeans1$cluster,kmeans2$cluster)
+
+## ----session_info--------------------------------------------------------
+devtools::session_info()
