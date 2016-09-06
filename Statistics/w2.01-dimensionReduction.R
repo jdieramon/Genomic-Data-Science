046651e4066f5b94ebd2e15cc830d4c22d57e797
jdieramon <jodiera@upv.es>
2016-09-05 21:37:07 -0400

    Create w2.01-dimensionReduction.R

---
+++ Statistics/w2.01-dimensionReduction.R
@@ -0,0 +1,102 @@
+## ----global_palette, results = 'asis'------------------------------------
+rm(list=ls())
+tropical=  c('darkorange', 'dodgerblue', 'hotpink', 'limegreen', 'yellow')
+palette(tropical)
+
+## ----global_options,warning=FALSE,message=FALSE--------------------------
+## see ch. 10 Hooks of Xie's knitr book
+library(knitr)
+knit_hooks$set(setPch = function(before, options, envir) {
+    if(before) par(pch = 19)
+})
+opts_chunk$set(setPch = TRUE)
+
+## ----global_plot,warning=FALSE, message=FALSE----------------------------
+knitr::opts_chunk$set(fig.width=5, fig.height=5, size="footnotesize",
+                      warning=FALSE, message=FALSE)
+knitr::knit_hooks$set(small.mar = function(before, options, envir) {
+    if (before) graphics::par(mar = c(5,5,1.5,1))
+})
+
+## ----load_hidden, echo=FALSE, results="hide", warning=FALSE--------------
+suppressPackageStartupMessages({
+    library(devtools)
+    library(Biobase)
+})
+
+## ----load----------------------------------------------------------------
+library(devtools)
+library(Biobase)
+
+## ----install_packages, eval=FALSE----------------------------------------
+#  install.packages(c("devtools"))
+#  source("http://www.bioconductor.org/biocLite.R")
+#  biocLite(c("Biobase"))
+
+## ------------------------------------------------------------------------
+con =url("http://bowtie-bio.sourceforge.net/recount/ExpressionSets/montpick_eset.RData")
+load(file=con)
+close(con)
+mp = montpick.eset
+pdata=pData(mp)
+edata=as.data.frame(exprs(mp))
+fdata = fData(mp)
+ls()
+
+## ------------------------------------------------------------------------
+edata = edata[rowMeans(edata) > 100, ]   #reduce number of rows
+edata = log2(edata + 1)                  #log2 transform
+edata_centered = edata - rowMeans(edata) #center the data
+svd1 = svd(edata_centered)
+names(svd1)
+
+## ------------------------------------------------------------------------
+plot(svd1$d,ylab="Singular value",col=2)
+plot(svd1$d^2/sum(svd1$d^2),ylab="Percent Variance Explained",col=2)
+
+## ------------------------------------------------------------------------
+par(mfrow=c(1,2))
+plot(svd1$v[,1],col=2,ylab="1st PC")
+plot(svd1$v[,2],col=2,ylab="2nd PC")
+
+## ------------------------------------------------------------------------
+plot(svd1$v[,1],svd1$v[,2],col=2,ylab="2nd PC",xlab="1st PC")
+
+## ------------------------------------------------------------------------
+plot(svd1$v[,1],svd1$v[,2],ylab="2nd PC",
+     xlab="1st PC",col=as.numeric(pdata$study), pch=19)
+
+# '''If you look in the PC1 axis, the two studies have very different 
+# ''' values of the PC. So it seems that one of the big sources of signals in
+# ''' the data set is which study the two data sets come from
+
+## ------------------------------------------------------------------------
+boxplot(svd1$v[,1] ~ pdata$study,border=c(1,2))
+points(svd1$v[,1] ~ jitter(as.numeric(pdata$study)),col=as.numeric(pdata$study))
+
+# '''there's a big difference in the PC1 between the Montgomery and the 
+# ''' Pickrell studies.
+
+## ------------------------------------------------------------------------
+pc1 = prcomp(edata)
+plot(pc1$rotation[,1],svd1$v[,1])
+
+### They are not exactly the same (they are not scaled in the same way)
+
+## ------------------------------------------------------------------------
+edata_centered2 = t(t(edata) - colMeans(edata)) #data centered by columns
+svd2 = svd(edata_centered2)
+plot(pc1$rotation[,1],svd2$v[,1],col=2)
+
+## ------------------------------------------------------------------------
+# Outliers can drive these decompositions
+edata_outlier = edata_centered
+edata_outlier[6,] = edata_centered[6,] * 10000
+svd3 = svd(edata_outlier)
+plot(svd1$v[,1],svd3$v[,1],xlab="Without outlier",ylab="With outlier")
+
+## ------------------------------------------------------------------------
+plot(svd3$v[,1],edata_outlier[6,],col=4)
+
+## ----session_info--------------------------------------------------------
+devtools::session_info()
