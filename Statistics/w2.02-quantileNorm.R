41a391f56db45cde39573109d3d30e7ad77b2923
Jose V. Die <jodiera@upv.es>
2020-03-20 18:21:43 +0100

    Update w2.02-quantileNorm.R

--- Statistics/w2.02-quantileNorm.R
+++ Statistics/w2.02-quantileNorm.R
@@ -1,3 +1,7 @@
+# quantile normalization
+
+Normalization : common distribution across samples
+
 We want to normalize the samples, so they have common distributions. 
 We´ll do this when we think the distributions are driven by technical variables. 
 
@@ -8,35 +12,36 @@ palette(tropical)
 
 ## ----global_options,warning=FALSE,message=FALSE--------------------------
 ## see ch. 10 Hooks of Xie's knitr book
-library(knitr)
-knit_hooks$set(setPch = function(before, options, envir) {
-    if(before) par(pch = 19)
-})
-opts_chunk$set(setPch = TRUE)
+# library(knitr)
+# knit_hooks$set(setPch = function(before, options, envir) {
+#   if(before) par(pch = 19)
+# })
+# opts_chunk$set(setPch = TRUE)
 
 ## ----global_plot,warning=FALSE, message=FALSE----------------------------
-knitr::opts_chunk$set(fig.width=5, fig.height=5, size="footnotesize",
-                      warning=FALSE, message=FALSE)
-knitr::knit_hooks$set(small.mar = function(before, options, envir) {
-    if (before) graphics::par(mar = c(5,5,1.5,1))
-})
+# knitr::opts_chunk$set(fig.width=5, fig.height=5, size="footnotesize",
+#                       warning=FALSE, message=FALSE)
+# knitr::knit_hooks$set(small.mar = function(before, options, envir) {
+#   if (before) graphics::par(mar = c(5,5,1.5,1))
+# })
 
 ## ----load_hidden, echo=FALSE, results="hide", warning=FALSE--------------
-suppressPackageStartupMessages({
-    library(devtools)
-    library(Biobase)
-    library(preprocessCore)
-})
+# suppressPackageStartupMessages({
+#   library(devtools)
+#   library(Biobase)
+#   library(preprocessCore)
+# })
 
 ## ----load----------------------------------------------------------------
 library(devtools)
 library(Biobase)
 library(preprocessCore)
 
-## ----install_packages, eval=FALSE----------------------------------------
-#  install.packages(c("devtools"))
-#  source("http://www.bioconductor.org/biocLite.R")
-#  biocLite(c("Biobase","preprocessCore"))
+## ----install_packages, Bioc v3.8 ----------------------------------------
+# install.packages(c("BiocManager"))
+# BiocManager::install(c("preprocessCore"))
+
+
 
 ## ------------------------------------------------------------------------
 con =url("http://bowtie-bio.sourceforge.net/recount/ExpressionSets/montpick_eset.RData")
@@ -46,9 +51,10 @@ mp = montpick.eset
 pdata=pData(mp)
 edata=as.data.frame(exprs(mp))
 fdata = fData(mp)
+rm(con,montpick.eset, mp)
 ls()
 
-## ------------------------------------------------------------------------
+## ----transform data and remove outlying values --------------------------
 edata = log2(edata + 1)
 dim(edata)
 edata = edata[rowMeans(edata) > 3, ]
@@ -59,7 +65,7 @@ plot(density(edata[,1]),col=colramp[1],lwd=3,ylim=c(0,.30))
 for(i in 2:20){lines(density(edata[,i]),lwd=3,col=colramp[i])}
 # these differences are probably due to technology and not biology
 
-## ------------------------------------------------------------------------
+## --quantilenormalization-------------------------------------------------
 norm_edata = normalize.quantiles(as.matrix(edata))
 plot(density(norm_edata[,1]),col=colramp[1],lwd=3,ylim=c(0,.20))
 for(i in 2:20){lines(density(norm_edata[,i]),lwd=3,col=colramp[i])}
@@ -68,7 +74,8 @@ for(i in 2:20){lines(density(norm_edata[,i]),lwd=3,col=colramp[i])}
 #quantiles for the very low and high values are difficultu to match up. 
 
 ## ------------------------------------------------------------------------
-# We have removes differences in variability but not the gene by gene variability.  So, we can study it. 
+# We have removes differences in variability but not the gene by gene 
+# variability.  So, we can study it. 
 plot(norm_edata[1,],col=as.numeric(pdata$study), pch=19)
 # We still have differences between the two studies. 
 
@@ -77,6 +84,10 @@ plot(norm_edata[1,],col=as.numeric(pdata$study), pch=19)
 svd1 = svd(norm_edata - rowMeans(norm_edata))
 plot(svd1$v[,1],svd1$v[,2],xlab="PC1",ylab="PC2",
      col=as.numeric(pdata$study), pch=19)
+
+# important : even though we've normalized out the total distribution, 
+# we can still have artifacts like batch effects or other types of artifacts 
+# in the data set. 
 
 ## ----session_info--------------------------------------------------------
 devtools::session_info()
